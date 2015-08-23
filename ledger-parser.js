6d5b678d5c3936a9212af275c060670e161bf6d6
Leonardo Melo <leomeloxp@gmail.com>
2015-08-23 09:40:03 +0100

    Add support for saving new entries
    Ledger parser now has support for adding new entries to your accounting
    file. Information on how to do this should be found in the readme file.
    
    For ease of use with a RESTful API implementation the method for adding
    new entries return a success/failure code (HTTP compliant) as well as a
    friendly message, eg "Sucess!". As of this version, this cannot be
    easily overriden yet, but will be made customisable in the near future.

--- ledger-parser.js
+++ ledger-parser.js
@@ -1,8 +1,12 @@
+"use strict";
 // Requiring dependencies
-var fs = require('fs');
+var fs = require("fs");
 
 // Beginning of code
-exports.recent = function(file){
+exports.file = "";
+exports.currency = "";
+
+exports.recent = function(){
     /*
      * Reads a file (fullpath) containing ledgerCLI records and parse it into a JSON
      * friendly format by creating a transaction object in the
@@ -11,15 +15,16 @@ exports.recent = function(file){
      * {date, consolidated, payee, postings{account, amount, currency}}
      */
     // Reads the ledger file and break it into lines
-    var ledgerFile   = fs.readFileSync(file, 'utf8'),
-        tArray       = ledgerFile.split('\n'),
+    var ledgerFile   = fs.readFileSync(exports.file, "utf8"),
         // Matches the transaction's first line
         RegDate      = /^(2[0-9]+\/[0-9]{2}\/[0-9]{2})(?:[ ]*)([\*\! ]{1})(?:[ ]*)([ a-z0-9\(\)\']+)/i,
         // Matches account and amount
-        RegPost      = /^(?:[ ]{2,})([\:a-z\-]*)[ ]*(£)([\.\-0-9]*)/i,
+        RegPost      = new RegExp("^(?:[ ]{2,})([\\:a-z\\-]*)(?:[ ]{2,}|)(" + exports.currency + "?)([\\.\\-0-9]*)", "i"),
+        // Creates an array with the sourced file breaking it by lines
+        tArray       = ledgerFile.split("\n"),
         // Creates the base variables
-        transactions = [],
-        transaction  = {};
+        transaction  = {},
+        transactions = [];
     // Loops through each line of the file to structure the object
     tArray.forEach(function(e) {
         if(e.match(RegDate)) {
@@ -38,7 +43,7 @@ exports.recent = function(file){
         else if (e.match(RegPost)) {
             var matched      = e.match(RegPost),
                 posting      = {};
-            posting.account  = matched[1];
+            posting.account  = matched[1].split(":");
             posting.currency = matched[2];
             posting.amount   = matched[3];
             transaction.postings.push(posting);
@@ -47,7 +52,39 @@ exports.recent = function(file){
 
     if (transaction.payee) {
         transactions.push(transaction);
+        transaction = {};
     }
 
     return transactions;
+};
+
+exports.add = function (transaction) {
+    if (
+        !transaction.date ||
+            !transaction.payee ||
+            !transaction.postings[0].account
+    ) {
+        return [400, "Can't add transaction, format not compatible"];
+    }
+
+    var tPostings   = [],
+        tString     = "\n";
+
+    tString += transaction.date + " ";
+    tString += transaction.consolidated + " ";
+    tString += transaction.payee + " \n";
+
+    transaction.postings.forEach( function(e){
+        var posting = "    ";
+        posting += e.account + "  ";
+        posting += e.currency;
+        posting += e.amount;
+        tPostings.push(posting);
+    });
+
+    tString += tPostings.join("\n");
+
+    fs.appendFileSync(exports.file, tString);
+
+    return [200, "Success!"];
 };
